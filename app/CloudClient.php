2f9036f4aab1f1063a6a9d8757c7c6bd6559f2d2
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-01-20 14:59:15 -0500

    create db schema

--- app/CloudClient.php
+++ app/CloudClient.php
@@ -6,6 +6,7 @@ use App\Dto\Application;
 use App\Dto\BackgroundProcess;
 use App\Dto\Command;
 use App\Dto\Database;
+use App\Dto\DatabaseCluster;
 use App\Dto\DatabaseType;
 use App\Dto\Deployment;
 use App\Dto\Domain;
@@ -345,7 +346,7 @@ class CloudClient
     }
 
     /**
-     * @return Paginated<Database>
+     * @return Paginated<DatabaseCluster>
      */
     public function listDatabases(): Paginated
     {
@@ -356,7 +357,7 @@ class CloudClient
         $responseData = $response->json();
 
         return new Paginated(
-            data: array_map(fn ($item) => Database::fromApiResponse($responseData, $item), $responseData['data'] ?? []),
+            data: array_map(fn ($item) => DatabaseCluster::fromApiResponse($responseData, $item), $responseData['data'] ?? []),
             links: $responseData['links'] ?? [],
         );
     }
@@ -373,11 +374,20 @@ class CloudClient
         return array_map(fn ($item) => DatabaseType::fromApiResponse($responseData, $item), $responseData['data'] ?? []);
     }
 
-    public function getDatabase(string $databaseId): Database
+    public function getDatabase(string $databaseId): DatabaseCluster
     {
         $this->include('schemas');
 
         $response = $this->client->get("/databases/{$databaseId}");
+
+        return DatabaseCluster::fromApiResponse($response->json());
+    }
+
+    public function createDatabase(string $clusterId, string $name): Database
+    {
+        $response = $this->client->post("/databases/clusters/{$clusterId}/databases", [
+            'name' => $name,
+        ]);
 
         return Database::fromApiResponse($response->json());
     }
