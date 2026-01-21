34514ed2cb1c2602235df4dff9282ccc8e4968e8
Joe Tannenbaum <joe.tannenbaum@laravel.com>
2026-01-21 12:14:09 -0500

    auto register renderers for prompts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,36 +4,11 @@ namespace App\Providers;
 
 use App\Middleware\CommandMiddlewareManager;
 use App\Middleware\RequiresAuthToken;
-use App\Prompts\Answered;
-use App\Prompts\ConfirmPromptRenderer;
-use App\Prompts\DataList;
-use App\Prompts\DataListRenderer;
-use App\Prompts\DynamicSpinner;
-use App\Prompts\MonitorDeployments;
-use App\Prompts\MonitorDeploymentsRenderer;
-use App\Prompts\MultiSelectPromptRenderer;
-use App\Prompts\NoteRenderer;
-use App\Prompts\PasswordPromptRenderer;
-use App\Prompts\SelectPromptRenderer;
-use App\Prompts\SlideIn;
-use App\Prompts\SlideInRenderer;
-use App\Prompts\SpinnerRenderer;
-use App\Prompts\TableRenderer;
-use App\Prompts\TextareaPromptRenderer;
-use App\Prompts\TextPromptRenderer;
 use Illuminate\Console\Events\CommandStarting;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
-use Laravel\Prompts\ConfirmPrompt;
-use Laravel\Prompts\MultiSelectPrompt;
-use Laravel\Prompts\Note;
-use Laravel\Prompts\PasswordPrompt;
 use Laravel\Prompts\Prompt;
-use Laravel\Prompts\SelectPrompt;
-use Laravel\Prompts\Spinner;
-use Laravel\Prompts\Table;
-use Laravel\Prompts\TextareaPrompt;
-use Laravel\Prompts\TextPrompt;
+use RuntimeException;
 use Symfony\Component\Console\ConsoleEvents;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
@@ -44,23 +19,25 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Prompt::addTheme('cloud', [
-            DynamicSpinner::class => SpinnerRenderer::class,
-            Note::class => NoteRenderer::class,
-            Spinner::class => SpinnerRenderer::class,
-            SlideIn::class => SlideInRenderer::class,
-            TextPrompt::class => TextPromptRenderer::class,
-            Answered::class => TextPromptRenderer::class,
-            SelectPrompt::class => SelectPromptRenderer::class,
-            MultiSelectPrompt::class => MultiSelectPromptRenderer::class,
-            PasswordPrompt::class => PasswordPromptRenderer::class,
-            ConfirmPrompt::class => ConfirmPromptRenderer::class,
-            TextareaPrompt::class => TextareaPromptRenderer::class,
-            MonitorDeployments::class => MonitorDeploymentsRenderer::class,
-            DataList::class => DataListRenderer::class,
-            Table::class => TableRenderer::class,
-        ]);
+        $renderers = collect(glob(app_path('Prompts/*.php')))
+            ->filter(fn ($file) => str_ends_with($file, 'Renderer.php'))
+            ->map(fn ($file) => str(basename($file))->replace('.php', '')->toString())
+            ->filter(fn ($class) => $class !== 'Renderer')
+            ->mapWithKeys(function ($class) {
+                $promptClass = str_replace('Renderer', '', $class);
+                $promptClass = collect([
+                    'App\\Prompts\\'.$promptClass,
+                    'Laravel\\Prompts\\'.$promptClass,
+                ])->first(fn ($class) => class_exists($class));
 
+                if (! $promptClass) {
+                    throw new RuntimeException('Prompt class not found for renderer: '.$class);
+                }
+
+                return [$promptClass => 'App\\Prompts\\'.$class];
+            });
+
+        Prompt::addTheme('cloud', $renderers->toArray());
         Prompt::theme('cloud');
 
         $this->registerCommandMiddleware();
